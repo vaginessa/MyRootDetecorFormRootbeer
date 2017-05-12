6b61d3f4150cbf8e9db6c940cc8fee1df0febc3e
poming\pomingpo <roy989898@gmail.com>
2017-05-12 11:08:37 +0800

    layout create

---
+++ app/src/main/java/com/example/pomingpo/myrootdetectorfromrootbeer/Constants.java
@@ -0,0 +1,19 @@
+package com.example.pomingpo.myrootdetectorfromrootbeer;
+
+/**
+ * Created by pomingpo on 2017/5/12.
+ */
+
+public class Constants {
+    public static String detectRootManagementApps = "Root Management Apps";
+    public static String detectPotentiallyDangerousApps = "PotentiallyDangerousApps ";
+    public static String detectTestKeys = "TestKeys ";
+    public static String checkForBusyBoxBinary = "BusyBoxBinary ";
+    public static String checkForSuBinary = "SU Binary ";
+    public static String checkSuExists = "2nd SU Binary check ";
+    public static String checkForRWPaths = "ForRWPaths ";
+    public static String checkForDangerousProps = "DangerousProps ";
+    public static String checkForRootNative = "Root via native check ";
+    public static String detectRootCloakingApps = "RootCloakingApps ";
+    public static String isSelinuxFlagInEnabled = "Selinux Flag Is Enabled ";
+}
--- app/src/main/java/com/example/pomingpo/myrootdetectorfromrootbeer/MainActivity.java
+++ app/src/main/java/com/example/pomingpo/myrootdetectorfromrootbeer/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.pomingpo.myrootdetectorfromrootbeer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.scottyab.rootbeer.RootBeer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
