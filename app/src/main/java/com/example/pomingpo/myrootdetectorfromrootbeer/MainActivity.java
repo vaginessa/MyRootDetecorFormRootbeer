77b206b7cab98e228f2fa9d06f68e45aeed02057
poming\pomingpo <roy989898@gmail.com>
2017-05-12 11:56:49 +0800

    set the content to the view

--- app/src/main/java/com/example/pomingpo/myrootdetectorfromrootbeer/MainActivity.java
+++ app/src/main/java/com/example/pomingpo/myrootdetectorfromrootbeer/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.pomingpo.myrootdetectorfromrootbeer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.pomingpo.myrootdetectorfromrootbeer.Task.BaseRootDetectTask;
-import com.scottyab.rootbeer.RootBeer;
+
+import java.util.HashMap;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvRootNative;
     private TextView tvRootCloakingApps;
     private TextView tvSelinuxFlagInEnabled;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +43,68 @@ public class MainActivity extends AppCompatActivity {
         tvRootNative = (TextView) findViewById(R.id.tv_RootNative);
         tvRootCloakingApps = (TextView) findViewById(R.id.tv_RootCloakingApps);
         tvSelinuxFlagInEnabled = (TextView) findViewById(R.id.tv_SelinuxFlagInEnabled);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        progressBar.setVisibility(View.VISIBLE);
+        new BaseRootDetectTask(this) {
+            @Override
+            protected void onPostExecute(HashMap<String, Boolean> stringBooleanHashMap) {
+                Set<String> keyset = stringBooleanHashMap.keySet();
+                for (String key : keyset) {
+
+                    setTheRootDetectedView(key, stringBooleanHashMap.get(key));
+
+                }
+
+                progressBar.setVisibility(View.GONE);
+            }
+        }.execute();
 
 
+    }
 
+    private void setTheRootDetectedView(String key, boolean detected) {
+        switch (key) {
+            case Constants.detectRootManagementApps:
+                tvRootManager.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.detectPotentiallyDangerousApps:
+                tvPotentiallyDangerousApps.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.detectTestKeys:
+                tvTestKeys.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.checkForBusyBoxBinary:
+                tvBusyBoxBinary.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.checkForSuBinary:
+                tvSuBinary.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.checkSuExists:
+                tvSuExists.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.checkForRWPaths:
+                tvRWPaths.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.checkForDangerousProps:
+                tvDangerousProps.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.checkForRootNative:
+                tvRootNative.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.detectRootCloakingApps:
+                tvRootCloakingApps.setText(booleanToDetectdString(detected));
+                break;
+            case Constants.isSelinuxFlagInEnabled:
+                tvSelinuxFlagInEnabled.setText(booleanToDetectdString(detected));
+                break;
+        }
+
+
+    }
+
+    private String booleanToDetectdString(boolean detected) {
+        return detected ? "detected" : "not detected";
 
     }
 }
